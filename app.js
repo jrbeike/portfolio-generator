bf5aa9f0b67bddb480dffc67d933730966c9be27
jrbeike <jrbeike@gmail.com>
2022-04-10 13:42:06 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+//console.log('hello node!');
+
+// var message = 'Hello Node!';
+
+// var sum = 5 + 3;
+
+// console.log(message);
+// console.log(sum);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+const message = 'Hello Node!';
+message = 'Goodbye Node!';
+
+const sum = 5 + 3;
+sum += 1;
